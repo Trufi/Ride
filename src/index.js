e805bf0ab3fdb8cef1f6f12236cbc75818d206f4
Mstislav Zhivodkov <stevemyz@gmail.com>
2018-06-19 17:05:13 +0700

    Add map controls

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ if ('serviceWorker' in navigator) {
         .catch((err) => console.log(err));
 }
 
-
 mapboxgl.accessToken = 'pk.eyJ1IjoidHJ1ZmkiLCJhIjoiY2lrcW1pdzU4MDEyanUwbTIwZzJ1bmY4dSJ9.5qW-pRcgah2nQIWpGKwHRg';
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/trufi/cjil6e9qp146w2sozewzwzlnk',
     center: [83.09598, 54.85404],
     zoom: 14,
+    attributionControl: false,
 });
 
+map.addControl(new mapboxgl.NavigationControl({
+    showZoom: false,
+}), 'top-right');
+
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+}), 'bottom-right');
+
+map.addControl(new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'metric',
+}), 'top-left');
